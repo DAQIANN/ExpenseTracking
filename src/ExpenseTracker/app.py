0d94e777c898af5607b601fcd84248e9170d621e
Daniel Qian <danielqian@Daniels-MacBook-Pro-2.local>
2023-05-21 23:50:51 -0700

    Finishing tutorial

--- src/ExpenseTracker/app.py
+++ src/ExpenseTracker/app.py
@@ -4,7 +4,13 @@ Allowing for tracking of expenses
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+import httpx
 
+def greeting(name):
+        if name:
+            return f"Hello, {name}"
+        else:
+            return "Hello, stranger"
 
 class ExpenseTrackerQian(toga.App):
 
@@ -16,12 +22,41 @@ class ExpenseTrackerQian(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box()
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+
+        name_label = toga.Label(
+            "Your name: ",
+            style=Pack(padding=(0, 5))
+        )
+        self.name_input = toga.TextInput(style=Pack(flex=1))
+
+        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        name_box.add(name_label)
+        name_box.add(self.name_input)
+
+        button = toga.Button(
+            "Say Hello!",
+            on_press=self.say_hello,
+            style=Pack(padding=5)
+        )
+
+        main_box.add(name_box)
+        main_box.add(button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+        
+    async def say_hello(self, widget):
+        async with httpx.AsyncClient() as client:
+            response = await client.get("https://jsonplaceholder.typicode.com/posts/42")
 
+        payload = response.json()
 
+        self.main_window.info_dialog(
+            greeting(self.name_input.value),
+            payload["body"],
+        )
+ 
 def main():
     return ExpenseTrackerQian()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,11 @@
+from ExpenseTracker.app import greeting
+
+def test_name():
+    assert greeting("Daniel") == "Hello, Daniel"
+
+def test_empty():
+    assert greeting("") == "Hello, stranger"
+
 def test_first():
     "An initial test for the app"
     assert 1 + 1 == 2
